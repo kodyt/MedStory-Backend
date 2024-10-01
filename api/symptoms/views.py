7923e6ac6ce5ce98eedba2aa6a74325855d6512a
Kody Takada <kodytakada@icloud.com>
2024-10-01 13:25:26 -0400

    Started integration

--- api/symptoms/views.py
+++ api/symptoms/views.py
@@ -16,10 +16,52 @@ def symptoms(request):
 def add_symptom_log(request):
     
     if request.method == 'POST':
-        # print(request.data)
+        print("Catching post request")
+        print(request.data)
+        symptom_data = {
+            "s_id": request.data["s_id"],
+            "severity": request.data["severity"],
+            "onset_time": request.data["onset_time"],
+            "type_of_pain": request.data["type_of_pain"],
+            "diagnosis": request.data["diagnosis"],
+            "notes": request.data["notes"],
+        }
 
-        serializer = PatientSymptomSerializer(data=request.data)
+        reminders = {
+            "frequency": int(request.data["freq"]),
+            "unit": request.data["remind_times"],
+            "text": "Example reminder text"
+        }
+
+        serializer = PatientSymptomSerializer(data=symptom_data)
         if serializer.is_valid():
             serializer.save()  # Save the data using the serializer's create method
             return Response({'message': 'Data saved successfully!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['POST'])
+# def add_symptom_numerical_log(request):
+    
+#     if request.method == 'POST':
+
+#         symptom_data = {
+#             "s_id": request.data["s_id"],
+#             "severity": request.data["severity"],
+#             "onset_time": request.data["time"],
+#             "type_of_pain": request.data["type_of_pain"],
+#             "diagnosis": request.data["diagnosis"],
+#             "notes": request.data["notes"],
+#         }
+
+#         reminders = {
+#             "frequency": int(request.data["freq"]),
+#             "unit": request.data["remind_times"],
+#             "text": "Example reminder text"
+#         }
+
+#         serializer = PatientSymptomSerializer(data=symptom_data)
+#         if serializer.is_valid():
+#             # serializer.save()  # Save the data using the serializer's create method
+#             return Response({'message': 'Data saved successfully!'}, status=status.HTTP_201_CREATED)
+        
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
